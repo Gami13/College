4708d49eecaf65cdde5dc0aedf403e155a09d664
Gami <gamiofficial0@gmail.com>
2025-10-28 16:14:18 +0100

    rename

---
+++ JiPP/Project1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void kolo(double r, double *w1, double *w2);
+
+int main(void) {
+  double r, w1, w2;
+  printf("Podaj promien kola: ");
+  int ret = scanf("%lf", &r);
+  if (ret != 1) {
+    printf("Blad podczas wczytywania danych.\n");
+    return 1;
+  }
+  if (r < 0) {
+    printf("Promien nie moze byc ujemny.\n");
+    return 1;
+  }
+
+  kolo(r, &w1, &w2);
+  printf("Pole kola: %.4lf\n", w1);
+  printf("Obwod kola: %.4lf\n", w2);
+
+  return 0;
+}
+
+void kolo(double r, double *w1, double *w2) {
+  *w1 = M_PI * r * r;
+  *w2 = 2 * M_PI * r;
+}
